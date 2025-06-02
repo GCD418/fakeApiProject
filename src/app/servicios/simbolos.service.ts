d24a19c6323c1f21c7349108b9c76b0ffa4b5f96
gcd418 <patosquare418@gmail.com>
2025-06-02 17:02:28 -0400

    feat: espero no haber arruinado nada

--- src/app/servicios/simbolos.service.ts
+++ src/app/servicios/simbolos.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Simbolo } from '../interfaces/simbolo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimbolosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obtenerTodosLosSimbolos():Observable<Simbolo[]> {
+    return this.http.get<Simbolo[]>('https://api.gemini.com/v1/symbols');
+  }
 }
