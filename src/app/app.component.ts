5bdf792b849cea18b221c9bf0bd6bcb40d167632
christian-ledezma <chris.ledezma.s@gmail.com>
2025-06-04 15:37:46 -0400

    Feat: pagina precios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SimbolosService } from './servicios/simbolos.service';
+import { PriceFeedService } from './servicios/price-feed.service';
+import { PriceFeedComponent } from './paginas/price-feed/price-feed.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, PriceFeedComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   simbolosService: SimbolosService = inject(SimbolosService);
+  priceFeedService: PriceFeedService = inject(PriceFeedService);
   constructor() {
   }
   obtenerSimbolos() {
@@ -23,4 +28,16 @@ export class AppComponent {
       }
     );
   }
+
+  obtenerPrices() {
+    this.priceFeedService.getPriceFeed().subscribe(
+      (data) => {
+        console.log('Datos de precios obtenidos:', data);
+      },
+      (error) => {
+        console.error('Error al obtener los precios:', error);
+      }
+    );
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PriceFeedComponent } from './paginas/price-feed/price-feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'precios', component: PriceFeedComponent }
+];
