0dc37eec66450f450619d2a91dc89b2c087b431b
gcd418 <patosquare418@gmail.com>
2025-06-04 14:54:03 -0400

    feat: no funcionan ni los simbolos aun

--- src/app/elementos/symbol/symbol.component.ts
+++ src/app/elementos/symbol/symbol.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Simbolo } from '../../interfaces/simbolo';
 
 @Component({
   selector: 'app-symbol',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './symbol.component.html',
   styleUrl: './symbol.component.scss'
 })
 export class SymbolComponent {
-
+  @Input() simbolo!: Simbolo;
 }
--- src/app/servicios/simbolos.service.ts
+++ src/app/servicios/simbolos.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Simbolo } from '../interfaces/simbolo';
+import { Simbolo, SimboloDetails } from '../interfaces/simbolo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimbolosService {
 
+  private baseUrl = 'https://api.sandbox.gemini.com/v1';
+
   constructor(private http: HttpClient) { }
 
   obtenerTodosLosSimbolos():Observable<Simbolo[]> {
-    return this.http.get<Simbolo[]>('https://api.gemini.com/v1/symbols');
+    return this.http.get<Simbolo[]>(`${this.baseUrl}/symbols`);
+  }
+
+  obtenerDetallesSimbolo(simbolo: string): Observable<SimboloDetails> {
+    return this.http.get<SimboloDetails>(`${this.baseUrl}/symbols/details/${simbolo}`);
   }
 }
